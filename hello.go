011e676533c87830c7d3ce5ec6a28ff7646cec1c
jiaqian1130 <jxu49220@gmail.com>
2023-08-16 02:20:29 +0800

    first commit

---
+++ hello.go
@@ -0,0 +1,15 @@
+package main 
+import "fmt"
+
+func main() {
+	//coding here
+	var x = 57
+if x==5 {
+		fmt.Println("x is 5")
+}else if x == 6{
+	fmt.Println("x is not 5 is 6")
+
+}else{
+	fmt.Println("x is not 5")
+}
+}
